495fc3cb582955aa493fc027e65051edda7a548c
divyanshukhare2001 <kharedivyanshu8@gmail.com>
2023-10-01 18:24:33 +0530

    adding file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 12345;
+let accountMail = "mail@mail.com";
+var accountBalance = "550";
+
+accountCity = "delhi";
+
+// accountId = 54321;    this is not allowed as accountId is constant
+accountMail = "js@mail.com";
+accountBalance = "500";
+
+let accountState;
+
+console.log(accountId);
+console.table([accountId , accountMail , accountBalance , accountCity , accountState]);
